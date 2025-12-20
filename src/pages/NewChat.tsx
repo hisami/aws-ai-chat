cb33a27f6bee4c6197d49dcc01e514cd41aa2963
hisami <koron.take@gmail.com>
2025-12-20 11:29:15 +0900

    add: 新規チャットのベース作成

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,7 +1,26 @@
-export default function App() {
+import { FaArrowUp } from "react-icons/fa";
+
+export default function NewChat() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">AIチャットアプリ</h1>
+    <div className="justiry-center flex h-screen items-center">
+      <div className="max-w-wl flex w-full flex-col gap-2">
+        <h1 className="text-center text-3xl font-bold">test-userさん</h1>
+        <form>
+          <div className="gep-4 flex flex-col justify-between rounded-2xl border border-gray-300 p-4">
+            <textarea
+              name="message"
+              className="field-sizing-content max-h-80 w-full resize-none overflow-y-auto border-none break-words outline-none"
+              placeholder="質問を入力してください"
+            />
+            <div className="flex justify-end gap-2">
+              {/** biome-ignore lint/a11y/useButtonType: <explanation> */}
+              <button className="disabled:hover-bg-emerald-600 flex items-center justify-center rounded-md bg-emerald-600 p-2 text-white hover:bg-emerald-700 disabled:opacity-50">
+                <FaArrowUp />
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
