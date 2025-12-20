b028354f299deffdad4aba0c4cb4712422be1593
hisami <koron.take@gmail.com>
2025-12-20 11:40:53 +0900

    add: modelの選択機能を実装

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,6 +1,21 @@
 import { FaArrowUp } from "react-icons/fa";
 
 export default function NewChat() {
+  const models = [
+    {
+      id: "1",
+      name: "Claude 4.5 Haiku v1",
+    },
+    {
+      id: "2",
+      name: "Claude Sonnet 4.5 v1",
+    },
+    {
+      id: "3",
+      name: "Amazon Nova Pro 1.0",
+    },
+  ];
+
   return (
     <div className="justiry-center flex h-screen items-center">
       <div className="max-w-wl flex w-full flex-col gap-2">
@@ -13,6 +28,15 @@ export default function NewChat() {
               placeholder="質問を入力してください"
             />
             <div className="flex justify-end gap-2">
+              <div className="jusity-center flex items-center rounded-md text-sm">
+                <select className="w-full rounded-md p-2">
+                  {models.map((model) => (
+                    <option key={model.id} value={model.id}>
+                      {model.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
               {/** biome-ignore lint/a11y/useButtonType: <explanation> */}
               <button className="disabled:hover-bg-emerald-600 flex items-center justify-center rounded-md bg-emerald-600 p-2 text-white hover:bg-emerald-700 disabled:opacity-50">
                 <FaArrowUp />
