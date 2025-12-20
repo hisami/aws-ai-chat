7b487e8d1b732ee03a446d21751de2fc55790d15
hisami <koron.take@gmail.com>
2025-12-20 16:23:28 +0900

    add: ログイン中のユーザ名を表示

--- src/components/ui/Profile.tsx
+++ src/components/ui/Profile.tsx
@@ -1,3 +1,4 @@
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { twMerge } from "tailwind-merge";
 
 interface ProfileProps {
@@ -5,9 +6,11 @@ interface ProfileProps {
 }
 
 export default function Profile({ className }: ProfileProps) {
+  const { user } = useAuthenticator((context) => [context.user]);
+  const username = user?.signInDetails?.loginId || "Guest";
   return (
     <div className={twMerge("mb-3 text-sm font-medium", className)}>
-      test-user@example.com
+      {username}
     </div>
   );
 }
--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,9 +1,12 @@
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { useNavigate } from "react-router";
 import ChatInput from "../components/ui/ChatInput";
 import { sampleConversations } from "../sampleData";
 import { createChatTitle } from "../utils";
 
 export default function NewChat() {
+  const { user } = useAuthenticator((context) => [context.user]);
+  const username = user?.signInDetails?.loginId || "Guest";
   const navigate = useNavigate();
   const sendMessage = (message: string, model: string) => {
     const conversationId = self.crypto.randomUUID();
@@ -34,7 +37,7 @@ export default function NewChat() {
   return (
     <div className="justiry-center flex h-screen items-center">
       <div className="max-w-wl flex w-full flex-col gap-2">
-        <h1 className="text-center text-3xl font-bold">test-userさん</h1>
+        <h1 className="text-center text-3xl font-bold">{username}さん</h1>
         <ChatInput sendMessage={sendMessage} />
       </div>
     </div>
