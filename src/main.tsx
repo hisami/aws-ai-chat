11bbe8a34c7d7c19a73bad83dc3d1612afb0bcb3
hisami <koron.take@gmail.com>
2025-12-20 16:11:57 +0900

    add: ログイン機能の実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
+import { Authenticator, translations } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import { I18n } from "aws-amplify/utils";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import outputs from "../amplify_outputs.json";
 import App from "./App.tsx";
+
+Amplify.configure(outputs);
+I18n.putVocabularies(translations);
+I18n.setLanguage("ja");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </StrictMode>,
 );
